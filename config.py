e7ae8a5dc51c16bbe13fdd0db191204140262909
huxuan <i@huxuan.org>
2014-02-12 21:46:24 +0800

    confi & requirements: add flask-wtf and its config

--- config.py
+++ config.py
@@ -7,4 +7,9 @@ Email: i(at)huxuan.org
 Description: config for app
 """
 
+# mysql / sqlalchemy
 SQLALCHEMY_DATABASE_URI = 'mysql://pkujishi:jishipku@localhost/pkujishi'
+
+# flask-wtf
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'pku-jishi-sk'

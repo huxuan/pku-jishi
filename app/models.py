bc6b00960a4d69021f280e014e411f639639a9b7
huxuan <i@huxuan.org>
2014-02-12 16:59:14 +0800

    app/models: pep8

--- app/models.py
+++ app/models.py
@@ -12,18 +12,18 @@ from app import db
 class User(db.Model):
     """docstring for User"""
 
-    id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(255), index = True, unique = True)
-    name = db.Column(db.String(255), unique = True, nullable = False)
-    password = db.Column(db.String(255), nullable = False)
-    confirm = db.Column(db.String(40), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), index=True, unique=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    confirm = db.Column(db.String(40), nullable=False)
     avatar = db.Column(db.String(255))
     phone = db.Column(db.String(40))
     qq = db.Column(db.String(40))
     create_time = db.Column(db.DateTime)
     status = db.Column(db.SmallInteger)
-    sells = db.relationship('Sell', backref='user', lazy = 'dynamic')
-    buys = db.relationship('Buy', backref='user', lazy = 'dynamic')
+    sells = db.relationship('Sell', backref='user', lazy='dynamic')
+    buys = db.relationship('Buy', backref='user', lazy='dynamic')
 
     def __repr__(self):
         """docstring for __repr__"""
@@ -32,13 +32,13 @@ class User(db.Model):
 class Category(db.Model):
     """docstring for Category"""
 
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(255), unique = True, nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
     order = db.Column(db.Integer)
     parent_id = db.Column(db.Integer)
     status = db.Column(db.SmallInteger)
-    sells = db.relationship('Sell', backref='category', lazy = 'dynamic')
-    buys = db.relationship('Buy', backref='category', lazy = 'dynamic')
+    sells = db.relationship('Sell', backref='category', lazy='dynamic')
+    buys = db.relationship('Buy', backref='category', lazy='dynamic')
 
     def __repr__(self):
         """docstring for __repr__"""
@@ -47,12 +47,12 @@ class Category(db.Model):
 class Location(db.Model):
     """docstring for location"""
 
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(255), unique = True, nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
     order = db.Column(db.Integer)
     status = db.Column(db.SmallInteger)
-    sells = db.relationship('Sell', backref='location', lazy = 'dynamic')
-    buys = db.relationship('Buy', backref='location', lazy = 'dynamic')
+    sells = db.relationship('Sell', backref='location', lazy='dynamic')
+    buys = db.relationship('Buy', backref='location', lazy='dynamic')
 
     def __repr__(self):
         """docstring for __repr__"""
@@ -65,8 +65,8 @@ class Sell(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
-    title = db.Column(db.String(30), nullable = False)
-    price = db.Column(db.Integer, nullable = False)
+    title = db.Column(db.String(30), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
     pics = db.Column(db.String(255))
     deprecate = db.Column(db.SmallInteger)
     description = db.Column(db.Text)

f502d85cb63c9186aedae0b0a978fd42417676a1
huxuan <i@huxuan.org>
2014-02-12 16:59:35 +0800

    db_schema.sql: bug fix in database creating

--- db_schema.sql
+++ db_schema.sql
@@ -1,3 +1,5 @@
 CREATE USER 'pkujishi'@'localhost' IDENTIFIED BY 'jishipku';
 GRANT ALL PRIVILEGES ON pkujishi . * TO 'pkujishi'@'localhost';
-CREATE DATABASE pkujishi;
+CREATE DATABASE pkujishi
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;

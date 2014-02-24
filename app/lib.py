d8c744248c10b3e528e41014a828cce25ec9900d
huxuan <i@huxuan.org>
2014-02-24 23:06:03 +0800

    app/lib: use sqlalchemy instead of flask-sqlalchemy

--- app/lib.py
+++ app/lib.py
@@ -69,23 +69,23 @@ def create_buy(user_id, title, price_low, price_high, category_id, location_id,
 
 def get_user_count():
     """docstring for get_user_count"""
-    return models.User.query.count()
+    return db.session.query(models.User).count()
 
 def get_locations(status=0):
     """docstring for get_locations"""
-    return models.Location.query.filter_by(status=status).all()
+    return db.session.query(models.Location).filter_by(status=status).all()
 
 def get_categories(status=0):
     """docstring for get_categories"""
-    return models.Category.query.filter_by(status=status).all()
+    return db.session.query(models.Category).filter_by(status=status).all()
 
 def get_category(id, status=0):
     """docstring for get_category"""
-    return models.Category.query.get(id)
+    return db.session.query(models.Category).get(id)
 
 def get_sell_count():
     """docstring for get_sell_count"""
-    return models.Sell.query.count()
+    return db.session.query(models.Sell).count()
 
 def get_sell_by_id(id):
     """docstring for get_sell_by_id"""
@@ -93,31 +93,30 @@ def get_sell_by_id(id):
 
 def get_sells_by_category(category, status=0):
     """docstring for get_sells_by_category"""
-    return models.Sell.query.filter_by(category=category, status=status).all()
+    return db.session.query(models.Sell).filter_by(category=category, status=status).all()
 
 def get_sells_by_user(user, status=0):
     """docstring for get_sells_by_user"""
-    return models.Sell.query.filter_by(user=user, status=status).all()
+    return db.session.query(models.Sell).filter_by(user=user, status=status).all()
 
 def get_sells_free(limit=4, status=0):
     """docstring for get_sells_free"""
-    return models.Sell.query.filter_by(price=0, status=status).\
+    return db.session.query(models.Sell).filter_by(price=0, status=status).\
         order_by(models.Sell.create_time.desc()).limit(limit).all()
 
 def get_sells_floors(categories, limit=4, status=0):
     """docstring for get_sells_floors"""
     floors = []
     for category in categories:
-        floor = models.Sell.query.\
+        floor = db.session.query(models.Sell).\
             filter_by(category=category, status=0).\
-            order_by(models.Sell.create_time.desc()).\
-            limit(limit).all()
+            order_by(models.Sell.create_time.desc())[:limit]
         floors.append(floor)
     return floors
 
 def get_sells_q_cid_lid(q, category_id=0, location_id=0):
     """docstring for get_sells_q_cid_lid"""
-    res = models.Sell.query.whoosh_search(q).filter_by(status=0)
+    res = db.session.query(models.Sell).whoosh_search(q).filter_by(status=0)
     if category_id != 0:
         res = res.filter_by(category_id=category_id)
     if location_id != 0:
@@ -126,7 +125,7 @@ def get_sells_q_cid_lid(q, category_id=0, location_id=0):
 
 def get_buy_count():
     """docstring for get_buy_count"""
-    return models.Buy.query.count()
+    return db.session.query(models.Buy).count()
 
 def get_buy_by_id(id):
     """docstring for get_buy_by_id"""
@@ -134,26 +133,25 @@ def get_buy_by_id(id):
 
 def get_buys_by_category(category, status=0):
     """docstring for get_buys_by_category"""
-    return models.Buy.query.filter_by(category=category, status=status).all()
+    return db.session.query(models.Buy).filter_by(category=category, status=status).all()
 
 def get_buys_by_user(user, status=0):
     """docstring for get_buys_by_user"""
-    return models.Buy.query.filter_by(user=user, status=status).all()
+    return db.session.query(models.Buy).filter_by(user=user, status=status).all()
 
 def get_buys_floors(categories, limit=4, status=0):
     """docstring for get_buys_floors"""
     floors = []
     for category in categories:
-        floor = models.Buy.query.\
+        floor = db.session.query(models.Buy).\
             filter_by(category=category, status=0).\
-            order_by(models.Buy.create_time.desc()).\
-            limit(limit).all()
+            order_by(models.Buy.create_time.desc())[:limit]
         floors.append(floor)
     return floors
 
 def get_buys_q_cid_lid(q, category_id=0, location_id=0):
     """docstring for get_buys_q_cid_lid"""
-    res = models.Buy.query.whoosh_search(q).filter_by(status=0)
+    res = db.session.query(models.Buy).whoosh_search(q).filter_by(status=0)
     if category_id != 0:
         res = res.filter_by(category_id=category_id)
     if location_id != 0:
